71b8e5e770aafa781538c4bb48e73dd9a4bf849a
xx52lw <liwenting@liwentingdeMacBook-Pro.local>
2018-03-15 13:50:08 +0800

    Initial Commit

---
+++ LWAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWAnimations
+//
+//  Created by liwei on 2018/3/15.
+//  Copyright © 2018年 LW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LWAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LWAnimations
+//
+//  Created by liwei on 2018/3/15.
+//  Copyright © 2018年 LW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
